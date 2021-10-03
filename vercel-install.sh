c32ee47d40bb621ce2f8786984acee2a9767cac4
Cedric GESTES <cedric.gestes@gmail.com>
2021-10-03 20:34:43 +0200

    fix vercel deployment script

--- vercel-install.sh
+++ vercel-install.sh
@@ -1,10 +1,12 @@
-#/bin/sh
+#!/bin/sh
+set -e
+set -x
 
 if cd flutter; then
     git pull && cd .. ;
 else
     git clone https://github.com/flutter/flutter.git;
-fi && ls && flutter/bin/flutter doctor && flutter/bin/flutter clean && flutter/bin/flutter config --enable-weif cd flutter; then git pull && cd .. ; else git clone https://github.com/flutter/flutter.git; fi && ls && flutter/bin/flutter doctor && flutter/bin/flutter clean && flutter/bin/flutter config --enable-web
+fi && ls && flutter/bin/flutter doctor && flutter/bin/flutter clean && flutter/bin/flutter config --enable-web
 
 if cd auto_route_library; then
     git pull && cd ..;
