0596f792deb4185b0b0288830084e96894762e94
Cedric GESTES <cedric.gestes@gmail.com>
2021-10-03 02:25:04 +0200

    fix all sort of issues. should work on all screens now

--- lib/main.dart
+++ lib/main.dart
@@ -13,59 +13,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Moonboard Circuits',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
       home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Moonboard circuits"), actions: <Widget>[
-        IconButton(
-          icon: const Icon(Icons.undo),
-          tooltip: 'Undo',
-          onPressed: () {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(const SnackBar(content: Text('Undo')));
-          },
-        ),
-        IconButton(
-          icon: const Icon(Icons.clear),
-          tooltip: 'Clear',
-          onPressed: () {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(const SnackBar(content: Text('Clear')));
-          },
-        ),
-        IconButton(
-          icon: const Icon(Icons.save_alt),
-          tooltip: 'Save',
-          onPressed: () {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(const SnackBar(content: Text('Save')));
-          },
-        ),
-        IconButton(
-          icon: const Icon(Icons.share),
-          tooltip: 'Share',
-          onPressed: () {
-            ScaffoldMessenger.of(context)
-                .showSnackBar(const SnackBar(content: Text('Share')));
-          },
-        ),
-      ]),
-      body: MoonboardLayout(),
-
-      // floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
